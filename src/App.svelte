eebd35867014fd3e0e8cc4718966eda531611d84
Zephrynis <zephrynis.yt@gmail.com>
2026-01-20 22:37:20 +0000

    Initialize Svelte + TypeScript + Vite project
    Set up a new Svelte project with TypeScript and Vite, including configuration files, basic app structure, recommended VS Code extension, and initial styling. Adds a simple 404 page with links, and configures scripts and dependencies for development and build.

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<h1>Page not Found</h1>
+<div class="row">
+  <a href="https://zephrynis.me/">Portfolio</a>
+  <a href="https://github.com/zephrynis">GitHub</a>
+  <a href="mailto:404@zephrynis.me">Contact</a>
+</div>
